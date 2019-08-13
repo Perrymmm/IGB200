4c2e0081866e1fc65066be862c801b48139c5110
Christian <cmilicevic3@gmail.com>
2019-08-13 12:16:40 +1000

    Added Project To Group File

---
+++ IGB200 Group Frog Game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform CameraTarget;
+
+    public float SmoothSpeed = 0.125f;
+
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = CameraTarget.position + offset;
+        Vector3 SmoothedPosition = Vector3.Lerp(transform.position, desiredPosition, SmoothSpeed);
+
+        transform.position = SmoothedPosition;
+
+        //transform.LookAt(CameraTarget); //View 2 Option
+    }
+}
---
+++ IGB200 Group Frog Game/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    public float speed;
+    
+
+    void Update()
+    {
+        //Player Rotate to Mouse
+
+        Plane PlayPlane = new Plane(Vector3.up, transform.position);
+        Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitdistance = 0.0f;
+
+            if (PlayPlane.Raycast(ray, out hitdistance))
+            {
+                Vector3 targetPoint = ray.GetPoint(hitdistance);
+                Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+                targetRotation.x = 0;
+                targetRotation.z = 0;
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 7f * Time.deltaTime);
+
+            }
+
+        //Player Movement 
+
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
